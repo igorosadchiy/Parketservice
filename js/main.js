6ee76e25004480f23f9c5e6a80beb1412f7c7869
igorosadchiy <37249430+igorosadchiy@users.noreply.github.com>
2018-09-25 14:41:25 +0300

    Add files via upload
    upload project

---
+++ js/main.js
@@ -0,0 +1,98 @@
+(function ($) {
+	"use strict";
+	
+	/*----------------------------
+    Responsive menu Active
+    ------------------------------ */
+	$(".mainmenu ul#primary-menu").slicknav({
+		allowParentLinks: true,
+		prependTo: '.responsive-menu',
+	});
+	
+	/*----------------------------
+    StickyHeader
+    ------------------------------ */
+	jQuery(window).on('scroll', function() {
+		if ($(this).scrollTop() > 10) {
+			$('.header').addClass("sticky");
+		} else {
+			$('.header').removeClass("sticky");
+		}
+	});
+	
+	/*----------------------------
+    Smooth scroll animation
+    ------------------------------ */
+	$('.mainmenu li a, .logo a,.slicknav_nav li a').on('click', function () {
+		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') &&
+            location.hostname === this.hostname) {
+		  var $target = $(this.hash);
+		  $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+		  if ($target.length) {
+			var targetOffset = $target.offset().top;
+			$('html,body')
+			.animate({scrollTop: targetOffset}, 2000);
+		   return false;
+		  }
+		}
+	});
+	
+	/*----------------------------
+    Scroll to Top
+    ------------------------------ */
+	$(window).on('scroll', function() {
+		if ($(this).scrollTop() > 600) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	$('.scrollToTop').on('click', function () {
+		$('html, body').animate({scrollTop : 0},2000);
+		return false;
+	});
+	
+	/*----------------------------
+    Slider activation
+    ------------------------------ */
+	$('.screenshot-wrap').slick({
+		autoplay: true,
+		dots: true,
+		autoplaySpeed: 5000,
+		slidesToShow: 3,
+		centerPadding: '20%',
+		centerMode: true,
+		prevArrow: '',
+		nextArrow: '',
+		responsive: [{
+
+		  breakpoint: 992,
+		  settings: {
+			slidesToShow: 1,
+			centerPadding: '33.3%'
+		  }
+
+		},{
+
+		  breakpoint: 576,
+		  settings: {
+			slidesToShow: 1,
+			centerPadding: '0'
+		  }
+
+		}]
+	});
+	
+	/*----------------------------
+    Preloader
+    ------------------------------ */
+	jQuery(window).on('load', function(){
+		jQuery("#preloader").fadeOut(500);
+	});
+    
+    /*----------------------------
+    Init WOW amimate
+    ------------------------------ */
+    new WOW().init();
+    
+}(jQuery));
